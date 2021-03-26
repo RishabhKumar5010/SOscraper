8c0f29003ef3ab4072675e1ac3c874a8288e29c3
RishabhKumar5010 <74526647+RishabhKumar5010@users.noreply.github.com>
2021-03-26 19:49:10 +0530

    remove debug print, add return

--- SOscrapper.py
+++ SOscrapper.py
@@ -1,24 +1,7 @@
 
-from selenium import webdriver
-from selenium.webdriver.firefox.webdriver import FirefoxProfile
-from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
 import re
-
-
-
-URL = 'https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do'
-
-
-
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-results = soup.find(id='question')
-
-#answers = results.find_all('div', class_='answers')
 
 def urlFilter(url,page=1):
     base = re.search(r'^.*?\d+',url)
@@ -32,6 +15,8 @@ def answer_data(url,soup):
     userinfotext = 'user-details'
     vote_sec = 'js-vote-count'
 
+    posts = []
+
     multipage = False
     length = 1
     try:
@@ -40,23 +25,28 @@ def answer_data(url,soup):
     except Exception as e:
         multipage = False
         print("Isn't a multipage prob.",e.args)
-        
-    for i in range(1,length+1):
-        if i!=1:
-            page = requests.get(urlFilter(url,i))
-            soup = BeautifulSoup(page.content, 'html.parser')
-            
-        for answer in soup.find_all("div", {"id" : answerid}):
+    try:
+        for i in range(1,length+1):
+            if i!=1:
+                page = requests.get(urlFilter(url,i))
+                soup = BeautifulSoup(page.content, 'html.parser')
+
+            for answer in soup.find_all("div", {"id" : answerid}):
+                post = {}
                 ansbody = answer.find("div",{'class':answerbody})
                 votes = answer.find('div',{'class':vote_sec})
-                #print('Post Body::\n==========\n'+ansbody.get_text()+'\n-x-x-x-x-x-x-\n')
+                post['body'] = ansbody.get_text()
                 user_sec = answer.find_all('div',{'class':postusers})[-1]
                 ans_by = user_sec.find('div',{'class':userinfotext}).a
-                print('Answer ID :',answer.attrs['data-answerid'])
-                print('Answered by :',ans_by.get_text())
-                print('Answerer ID :',answer.attrs['data-ownerid'])
-                print('Votes :',votes.get_text())
-            
+                post['post_id'] = answer.attrs['data-answerid']
+                post['op_name'] = ans_by.get_text()
+                post['op_id'] = answer.attrs['data-ownerid']
+                post['votes'] = votes.get_text()
+                posts.append(post)
+    except Exception as e:
+        print('In answer_data :',e.args)
+    return posts
+
 
 
 def question_data(soup):
@@ -67,38 +57,30 @@ def question_data(soup):
     userinfotext = 'user-details'
     vote_sec = 'js-vote-count'
     
+    post = {}
+    
     question = soup.find('div',questionid)
     quesbody = question.find("div",{'class':quesbody})
     votes = question.find('div',{'class':vote_sec})
-    #print('Post Body::\n==========\n'+ansbody.get_text()+'\n-x-x-x-x-x-x-\n')
+    post['body'] = quesbody.get_text()
     user_sec = question.find_all('div',{'class':postusers})[-1]
     ques_by = user_sec.find('div',{'class':userinfotext}).a
-    print('Question ID :',question.attrs['data-questionid'])
-    print('Asked by :',ques_by.get_text())
-    print('Asker ID :',question.attrs['data-ownerid'])
-    print('Votes :',votes.get_text())
+    post['post_id'] = question.attrs['data-questionid']
+    post['op_name'] = ques_by.get_text()
+    post['op_id'] = question.attrs['data-ownerid']
+    post['votes'] = votes.get_text()
+    
+    return post
+
             
+if __name__=='__main__':
+    URL = 'https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do'
 
-URL = 'https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-question_data(soup)
-answer_data(URL,soup)
-
-
-'''
-profile = FirefoxProfile("/home/galaxy/.mozilla/firefox/thmcw7ac.default-release")
-driver = webdriver.Firefox(profile)
-
-
-try:
-    driver.get("https://google.com")
+    quest = question_data(soup)
+    ans = answer_data(URL,soup)
+    print(len(ans))
     
-    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-    driver.get("https://stackoverflow.com")
-    
-except Exception as e:
-    print(e.args)
-'''
+
